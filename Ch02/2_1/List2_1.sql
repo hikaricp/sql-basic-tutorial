62da4bac9d3aafcd2a516e5a6c91a360338526e1
hikaricp <dockerc@163.com>
2024-01-03 19:38:49 +0800

    从 Produt 表中输出 3 列

---
+++ Ch02/2_1/List2_1.sql
@@ -0,0 +1,14 @@
+SELECT product_id, product_name, purchase_price FROM Product;
+
+-- 输出结果
+--  product_id | product_name | purchase_price
+-- ------------+--------------+----------------
+--  0001       | T恤衫        |            500
+--  0002       | 打孔器       |            320
+--  0003       | 运动T恤      |           2800
+--  0004       | 菜刀         |           2800
+--  0005       | 高压锅       |           5000
+--  0006       | 叉子         |
+--  0007       | 擦彩板       |            790
+--  0008       | 圆珠笔       |
+-- (8 rows)
