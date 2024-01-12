786047b946785908fa7e9b271c614443734eaa63
hikaricp <dockerc@163.com>
2024-01-12 22:30:16 +0800

    算数和比较运算符

---
+++ Ch02/2_2/List2_18.sql
@@ -0,0 +1,12 @@
+SELECT
+    product_name,
+    product_type
+FROM
+    Product
+WHERE
+    sale_price = 500;
+
+-- --------------+--------------
+--  打孔器       | 办公用品
+--  叉子         | 厨房用具
+-- (2 rows)
